be687d99933c0d4aead81cbb5e502419ff2c5aea
Jonathan_Alorda <Jonathan_Alorda@Dellteam.com>
2020-05-07 13:55:50 -0500

    Started adding const desclarations to the .js file.

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,7 @@
+// Get elements
+const player = document.querySelector('.player')
+const video = player.querySelector('.viewer')
+const progress = querySelector('progress')
+const progressBar = querySelector('')
+const progress = .querySelector('')
+const progress = .querySelector('')
