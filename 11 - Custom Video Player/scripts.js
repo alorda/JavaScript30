645103f4633d415752951c7b3d6837d03c992ad0
Jonathan_Alorda <Jonathan_Alorda@Dellteam.com>
2020-06-10 17:20:29 -0500

    Started with code for the video player.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,7 +1,24 @@
 // Get elements
 const player = document.querySelector('.player')
-const video = player.querySelector('.viewer')
-const progress = querySelector('progress')
-const progressBar = querySelector('')
-const progress = .querySelector('')
-const progress = .querySelector('')
+// const video = player.querySelector('.viewer')
+const video = player.querySelector('.player__video')
+const progress = player.querySelector('progress')
+const progressBar = player.querySelector('.progress__filled')
+const toggle = player.querySelector('.toggle')
+const skipButtons = player.querySelectorAll('[data-skip]')
+const ranges = player.querySelectorAll('.player__slider')
+
+// Functions
+function togglePlay() {
+  console.log(video);
+  if (video.paused) {
+    video.play()
+  } else {
+    video.pause()
+  }
+}
+
+// Event Listeners
+
+video.addEventListener('click', (e) => togglePlay(e))
+toggle.addEventListener('click', (e) => togglePlay(e))
